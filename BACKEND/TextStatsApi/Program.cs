6cc9ea5abb4086b6538bdff0f23b1fa0a14b737c
filemat <fmate@stud.uni-obuda.hu>
2025-04-18 20:12:51 +0200

    Configure backend, in order to allow Controllers, and enabling CORS

--- BACKEND/TextStatsApi/Program.cs
+++ BACKEND/TextStatsApi/Program.cs
@@ -1,6 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors("AllowAll");
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
