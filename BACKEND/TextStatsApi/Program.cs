c005317af6db10618c19a9e4feac24ae2c324bb1
filemat <fmate@stud.uni-obuda.hu>
2025-05-08 20:17:21 +0200

    Add feature to auto open the html file for ease of use

--- BACKEND/TextStatsApi/Program.cs
+++ BACKEND/TextStatsApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.FileProviders;
 using TextStatsApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +21,28 @@ builder.Services.AddCors(options =>
 builder.Services.AddScoped<ITextAnalysisService, TextAnalysisService>();
 
 var app = builder.Build();
+
+app.UseRouting();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}"
+    );
+
+var frontendPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "..",  "FRONTEND"));
+Console.WriteLine(AppContext.BaseDirectory);
+
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(frontendPath),
+    RequestPath = ""
+});
+
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/index.html");
+    return Task.CompletedTask;
+});
 
 app.UseSwagger();
 app.UseSwaggerUI();
